528f1de242cf57fbdecda26d9b6da0107225e877
amr-yassir <amryassir66@gmail.com>
2023-08-25 16:29:12 +0000

    task

--- _specifier.c
+++ _specifier.c
@@ -21,7 +21,22 @@ int _specifier(char specifier, va_list p)
 			break;
 		case 'd':
 		case 'i':
-			ctr += print_dig((long)va_arg(p, int));
+			ctr += print_dig((long)va_arg(p, int), 10);
+			break;
+		case 'b':
+			ctr += print_dig((long)va_arg(p, unsigned int), 2);
+			break;
+		case 'u':
+			ctr += print_p_dig((long)va_arg(p, int));
+			break;
+		case 'o':
+			ctr += print_dig((long)va_arg(p, int), 8);
+			break;
+		case 'x':
+			ctr += print_dig((long)va_arg(p, int), 16);
+			break;
+		case 'X':
+			ctr += print_dig((long)va_arg(p, int), 16);
 			break;
 		default:
 			ctr += write(1, &specifier, 1);
--- print_dig.c
+++ print_dig.c
@@ -1,28 +1,42 @@
 #include "main.h"
 
 /**
- * print_dig - prints a decimal number
+ * print_dig - prints digit based on base
  * @n: input
+ * @base: the base
  * Return: size of output
 */
 
-int print_dig(long n)
+int print_dig(long n, int base)
 {
 	int ctr;
-	char *s = "0123456789";
+	char *s = "0123456789abcdef";
 
 	if (n < 0)
 	{
 		write(1, "-", 1);
-		return (print_dig(-n) + 1);
+		return (print_dig(-n, base) + 1);
 	}
-	else if (n < 10)
+	else if (n < base)
 	{
 		return (print_char(s[n]));
 	}
 	else
 	{
-		ctr = print_dig(n / 10);
-		return (ctr + print_dig(n % 10));
+		ctr = print_dig(n / base, base);
+		return (ctr + print_dig(n % base, base));
 	}
+}
+
+/**
+ * print_p_dig - convert int to unsigned int
+ * @n: input
+*/
+
+int print_p_dig(long n)
+{
+	if (n < 0)
+		return (print_dig(-n, 10));
+	else
+		return (print_dig(n, 10));
 }
